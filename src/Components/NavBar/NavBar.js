ac44ffdba5fc7f2df3115a757b24ff499fa8d257
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-04-15 14:38:13 -0600

    Add functionality to download resume button

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -4,16 +4,18 @@ import { LuDownload } from 'react-icons/lu';
 
 const NavBar = () => {
   return (
-    <nav className='nav' >
+    <nav className='nav'>
       <p className='logo-name'>Cameron Johnson</p>
       <ul className='nav-links'>
         <li className='nav-link'>Work</li>
         <li className='nav-link'>Skills</li>
         <li className='nav-link'>Contact</li>
       </ul>
-      <button className='resume-button'>
-        Download Resume <LuDownload />
-      </button>
+      <a download href='https://docs.google.com/document/d/1BI8LlPgUVaegaGtnUVCliXjBjVazeJMdAFI1U3NPuK4/export?format=pdf'>
+        <button className='resume-button'>
+          Download Resume <LuDownload />
+        </button>
+      </a>
     </nav>
   );
 };
