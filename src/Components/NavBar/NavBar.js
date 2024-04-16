f83e13bf1d095c951392030adb3c7bda9e060518
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-04-15 21:14:48 -0600

    Add dynamic text to download button

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NavBar.css';
 import { LuDownload } from 'react-icons/lu';
 
 const NavBar = () => {
+  const [downloadMessage, setDownloadMessage] = useState('Download Resume');
+  const handleDownload = () => {
+    setDownloadMessage('Downloaded!');
+    setTimeout(() => {
+      setDownloadMessage('Download Resume');
+    }, 3000);
+  };
+
   return (
     <nav className='nav'>
       <p className='logo-name'>Cameron Johnson</p>
@@ -11,9 +19,13 @@ const NavBar = () => {
         <li className='nav-link'>Skills</li>
         <li className='nav-link'>Contact</li>
       </ul>
-      <a download href='https://docs.google.com/document/d/1BI8LlPgUVaegaGtnUVCliXjBjVazeJMdAFI1U3NPuK4/export?format=pdf'>
+      <a
+        onClick={handleDownload}
+        download
+        href='https://docs.google.com/document/d/1BI8LlPgUVaegaGtnUVCliXjBjVazeJMdAFI1U3NPuK4/export?format=pdf'
+      >
         <button className='resume-button'>
-          Download Resume <LuDownload />
+          {downloadMessage} <LuDownload />
         </button>
       </a>
     </nav>
