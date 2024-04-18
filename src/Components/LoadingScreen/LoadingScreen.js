465d4972203be81c0e124f7516379b98430fbb71
Cam Johnson <cameron.n.johnson15@gmail.com>
2024-04-18 12:23:32 -0600

    Add loading screen

--- src/Components/LoadingScreen/LoadingScreen.js
+++ src/Components/LoadingScreen/LoadingScreen.js
@@ -1,10 +1,42 @@
 import React, { useState, useEffect } from 'react';
 import './LoadingScreen.css';
+import { motion, AnimatePresence } from 'framer-motion';
 
-const LoadingScreen = ({loadingScreenMessage, loading}) => {
+const LoadingScreen = ({ loading }) => {
+  const [showWelcome, setShowWelcome] = useState(false);
+
+  useEffect(() => {
+    if (!loading) {
+      setTimeout(() => {
+        setShowWelcome(true);
+      }, 500);
+    } else setShowWelcome(false);
+  }, [loading]);
+
   return (
     <div className='loading-screen'>
-      <h1 className='loading-text'>{loadingScreenMessage}</h1>
+      <AnimatePresence>
+        {loading && (
+          <motion.h1
+            key='loading'
+            initial={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            className='loading-text'
+          >
+            Loading...
+          </motion.h1>
+        )}
+        {showWelcome && (
+          <motion.h1
+            key='welcome'
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            className='loading-text'
+          >
+            Welcome!
+          </motion.h1>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
